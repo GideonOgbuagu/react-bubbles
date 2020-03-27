6577e9779c4da5a989dde16a24c7a587a77acfd4
Gideon <gideon@mac-1s-mbp.myfiosgateway.com>
2020-03-27 13:12:23 -0400

    making successful post requests, received token from server.js & stored it in localStorage

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 const Login = () => {
   const [state, setState] = useState({
@@ -15,16 +16,27 @@ const Login = () => {
     //console.log(state)
   }
   // make a post request to retrieve a token from the api
-
+ const handleSubmit = e => {
+   e.preventDefault()
+   axiosWithAuth()
+        .post("/api/login", state)
+        .then(res => {
+          console.log("FROM LOGIN!!!", res)
+          localStorage.setItem('token', JSON.stringify(res.data.payload))
+        })
+        .catch(err => {
+          console.log("THROWING ERROR FROM LOGIN: ", err)
+        })
+ }
   // when you have handled the token, navigate to the BubblePage route
   return (
     <div className="login-cont">
           <div className="login-header">
             <h1>Welcome to the Bubble App!</h1>
-            <p>Login</p>
+            <p>Continue with username and password</p>
           </div>
           <div className="form-cont">
-              <form className="form">
+              <form className="form" onSubmit={handleSubmit}>
                   <input type="text" name="username" value={state.username} onChange={handleChange} placeholder="Enter Username"/>
                   <input type="password" name="password" value={state.password} onChange={handleChange} placeholder="Enter Password"/>
                   <button className="login-button">Login</button>
